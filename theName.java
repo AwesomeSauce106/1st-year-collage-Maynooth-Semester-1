b62d15b68441187753bf43af93b8983b1b64444e
AwesomeSauce106 <90049146+AwesomeSauce106@users.noreply.github.com>
2024-01-01 21:08:41 +0000

    Add files via upload

---
+++ theName.java
@@ -0,0 +1,8 @@
+public class theName {
+
+    public static void main(String[] args) {
+        System.out.print("John Doe");
+        System.out.println("John Doe");
+        System.out.println("John Doe");
+    }
+}
